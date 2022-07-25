ddff80f1be209708ef4385ddb7a179cc38124688
forxame <patlatidan1@gmail.com>
2022-07-25 03:11:02 -0700

    Added 2 new features

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
 const container = document.getElementById("container");
-let slider = document.getElementById("slider");
-let sliderValue = document.querySelectorAll("#slider-value");
+const slider = document.getElementById("slider");
+const sliderValue = document.querySelectorAll("#slider-value");
 const clearBtn = document.getElementById("clear-btn");
-let color = document.getElementById("color")
+const eraserBtn = document.getElementById("eraser-btn");
+const rainbowBtn = document.getElementById("rainbow-btn");
+const colorBtn = document.getElementById("color-btn");
+const color = document.getElementById("color");
+const activeMode = document.querySelector(".active");
+
+let currentMode = "color";
+colorBtn.classList.add("active");
+
+let isDrawing = false;
+document.body.onmousedown = () => (isDrawing = true);
+document.body.onmouseup = () => (isDrawing = false);
+
+eraserBtn.addEventListener("click", ()=> changeMode("eraser"));
+rainbowBtn.addEventListener("click", ()=> changeMode("rainbow"));
+colorBtn.addEventListener("click", ()=> changeMode  ("color"));
+
+function changeMode(newMode) {
+  if (currentMode === "eraser") {
+    eraserBtn.classList.remove("active");
+  } else if (currentMode === "rainbow") {
+    rainbowBtn.classList.remove("active");
+  } else if (currentMode === "color") {
+    colorBtn.classList.remove("active");
+  }
+
+  if (newMode === "eraser") {
+    eraserBtn.classList.add("active");
+  } else if (newMode === "rainbow") {
+    rainbowBtn.classList.add("active");
+  } else if (newMode === "color") {
+    colorBtn.classList.add("active");
+  }
+  
+  currentMode = newMode;
+}
+
+function startStopDraw(e) {
+  if (e.type === "mouseover" && !isDrawing) return;
+  if (currentMode === "color") {
+    e.target.style.backgroundColor = color.value;
+  } else if (currentMode === "rainbow") {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+  } else if (currentMode === "eraser") {
+    e.target.style.backgroundColor = "white";
+  }
+}
 
 function resetSize(size) {
 container.style.setProperty('--grid-rows', size);
@@ -16,20 +66,11 @@ function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
-    
-    
-        function startDrawing(e) {
-            cell.addEventListener("mouseover", ()=>{
-                cell.style.backgroundColor = color.value;
-            })
-        }
+  
 
-        function stopDrawing(e) {
-            cell.removeEventListener("mouseover")
-        }
-
-    container.addEventListener("mousedown", startDrawing);
-    // container.addEventListener("mouseup", stopDrawing);
+    container.addEventListener("mouseover", startStopDraw);
+    container.addEventListener("mousedown", startStopDraw);
+      
 
     function clearGrid() {
       clearBtn.addEventListener("click", ()=>{
